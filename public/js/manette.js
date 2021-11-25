21a5be1bb8ac79e49a93d99246bfb053abc87bfd
Abdelfettah <abdelfettah.youbi@gmail.com>
2021-11-25 22:43:12 +0100

    Fix manette + envoie des sockets des mouvements des 2 joysticks

--- public/js/manette.js
+++ public/js/manette.js
@@ -1,47 +1,9 @@
 const socket = io();
 
-const config = {
-    type: Phaser.AUTO,
-    width : 800,
-    height : 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {y: 0}
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-}
-
-var game = new Phaser.Game(config);
-
-var move;
-var aim;
-var direction;
-var pression;
-
-function preload ()
-{
-    /* load les images qu'il faudra DL je pense */
-}
-
-function create ()
-{
-    ecranConnexion();
-    move = new JoyStick('move');
-    aim = new JoyStick('aim');
-}
-
-function update () 
-{
-   if( (move.getDir) != "C")
-        socket.emit("mouvement", move.GetX, move.GetY); 
-    
-    }
+let move;
+let aim;
+let direction;
+let pression;
 
 function ecranConnexion() {
     socket.emit("firstConnection", "manette");
@@ -52,5 +14,85 @@ function ecranConnexion() {
     boutonEntrer.addEventListener("click", () => {
         socket.emit("manetteLogin", champLogin.value);
         document.getElementById("form").style.display = "none";
+        document.getElementById("move").style.visibility = "visible";
+        document.getElementById("aim").style.visibility = "visible";
     });
 }
+
+function manette()
+{
+    move = new JoyStick("move", {
+        title: "joystick",
+
+        // width/height
+        width: undefined,
+        height: undefined,
+
+        // Internal color of Stick
+        internalFillColor: "#3381ff",
+
+        // Border width of Stick
+        internalLineWidth: 2,
+
+        // Border color of Stick
+        internalStrokeColor: "#3381ff",
+
+        // External reference circonference width
+        externalLineWidth: 2,
+
+        //External reference circonference color
+        externalStrokeColor: "#3381ff",
+
+        // Sets the behavior of the stick
+        autoReturnToCenter: true,
+    });
+
+    aim = new JoyStick("aim", {
+        title: "joystick",
+
+        // width/height
+        width: undefined,
+        height: undefined,
+
+        // Internal color of Stick
+        internalFillColor: "#ff3333",
+
+        // Border width of Stick
+        internalLineWidth: 2,
+
+        // Border color of Stick
+        internalStrokeColor: "#ff3333",
+
+        // External reference circonference width
+        externalLineWidth: 2,
+
+        //External reference circonference color
+        externalStrokeColor: "#ff3333",
+
+        // Sets the behavior of the stick
+        autoReturnToCenter: true,
+    });
+
+    let loop = setInterval( () => {
+        if((move.GetDir()) != "C")
+        {
+            // console.log("test : X ", + move.GetX() + " Y : " + move.GetY());
+            socket.emit("mouvementMove", move.GetX(), move.GetY());
+        } 
+        else if((aim.GetDir()) != "C") 
+        {
+            // console.log("test : X ", + aim.GetX() + " Y : " + aim.GetY());
+            socket.emit("mouvementAim", aim.GetX(), aim.GetY());
+        } 
+    }, 2)
+
+    return loop
+}
+
+function main() 
+{
+    ecranConnexion();
+    manette();
+}
+
+window.onload = main;
--- server/server.js
+++ server/server.js
@@ -3,17 +3,13 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 const { text } = require("express");
-require("@geckos.io/phaser-on-nodejs")
-const Phaser = require("phaser")
 
 const publicPATH = path.join(__dirname, "../public");
 // const node_mPATH = path.join(__dirname, "../node_modules");
 const fichierManette = publicPATH + "/manette.html";
 const fichierEcran = publicPATH + "/ecran.html";
 
-const port = 3000;
-let user;
-let userLogin;
+const port = 4000;
 
 // Creation de l'objet joueurs dans laquelle on va stocker toutes les infos de chaque joueur
 let joueurs = {};
@@ -35,80 +31,20 @@ app.get("/ecran", (req, res) => {
 	res.sendFile(fichierEcran);
 })
 
-class ServerScene extends Phaser.Scene {
-	constructor(){
-		super();
-	}
-}
-
-
-class Tank extends Phaser.Physics.Arcade.Sprite {
-	constructor(scene, x, y, vie, degats, nom, equipe){
-		super(scene, x, y, '')
-
-		this.vie = vie;
-		this.degats = degats;
-		this.nom = nom;
-		this.equipe = equipe;
-
-		scene.add.existing(this)
-    	scene.physics.add.existing(this)
-
-    	this.body.setSize(100, 100)
-    	this.setCollideWorldBounds(true)
-	}
-}
-
-
-	function create() {
-		this.physics.world.setBounds(0, 0, 1280, 720);
-
-		io.on("connection", socket => {
-			const tank = new Tank(this, 100, 200);
-
-			this.tanks[socket.id].socket = socket;
-			this.tanks[socket.id].joueur = tank;
-		})
-
-		
-	}
-
-
-const config = {
-	type: Phaser.HEADLESS,
-	width: 1280,
-	height: 720,
-	banner: false,
-	audio: false,
-	scene: [ServerScene],
-	physics: {
-	  default: 'arcade',
-	  arcade: {
-		gravity: { y: 50 }
-	  }
-	}
-}
-
-new Phaser.Game(config)
-
 function manette(socket) 
 {
 	socket.on("manetteLogin", (pseudo) => {
 		    // Creation d'un joueur d'objet joueurs avec une postition random, login et id
 			joueurs[socket.id] = {
-				rotation: 0,
+				// rotation: 0,
+				angle: 0,
 				x: Math.floor(Math.random() * 700) + 50,
 				y: Math.floor(Math.random() * 500) + 50,
 				id: socket.id,
 				nickname: pseudo,
-			};	
+			};
+			// console.table(joueurs)
 	})
-}
-
-function mouvement(id, x, y)
-{
-	joueurs[id].x += x;
-	joueurs[id].y += y;
 }
 
 function ecran(socket) 
@@ -136,8 +72,20 @@ io.on("connection", (socket) => {
         }
     });
 
-	socket.on("mouvement", (x, y) => {
-		mouvement(socket.id, x, y);
+	socket.on("mouvementMove", (x, y) => {
+		joueurs[socket.id].x = x;
+		joueurs[socket.id].y = y;
+	})
+
+	socket.on("mouvementAim", (x, y) => {
+		// console.log("AIM : X = " + x + " Y = " + y);
+		let rad = Math.atan2(y, x);
+		let deg = rad * (180 / Math.PI)
+		// let deg2 = (Math.atan2(y, x)) * (180 / Math.PI)
+		// deg2 = deg2 * (2 * Math.PI)
+		joueurs[socket.id].angle = deg
+
+		// console.log(joueurs[socket.id]);
 	})
 
     // On supprime un joueur de l'objets joueurs quand il se deconnecte
@@ -153,7 +101,6 @@ io.on("connection", (socket) => {
 		}
     });
 });
-
 
 server.listen(port, () => {
     console.log(`Listening on ${server.address().port}`);
